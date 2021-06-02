b8894573b0863fa7781895757e55f8f3ad1305e0
Muntsa <msr@tinet.org>
2021-06-02 12:39:14 +0200

    app modified

--- App.js
+++ App.js
@@ -9,10 +9,7 @@ import ColoredScreen from './components/ColoredScreen';
     Create a new Screen component (call it ColoredScreen.js) which is an empty <View/> with background color as a parameter.
     In App.js add two ColoredButtons with texts ("Blue Screen", "Yellow Screen"). If you click first button you will show a Blue Screen, second button you will show a Yellow Screen.
   */
-  
-  <ColoredButton onPress={ColoredScreen} color="blue" text="Blue Screen"/>
-<ColoredButton onPress={ColoredScreen} color="yellow" text="Yellow Screen"/>
-
+ 
 
     
 export default function App() {
