0ea08dad005448729b0075e5153390dbab9b1966
Muntsa <msr@tinet.org>
2021-06-02 11:51:31 +0200

    falta parent element

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { View } from 'react-native';
 import Screen1 from './screens/Screen1';
+import ColoredButton from './components/ColoredButton';
+import ColoredScreen from './components/ColoredScreen'
 
 //Exercise 3
   /*
     Create a new Screen component (call it ColoredScreen.js) which is an empty <View/> with background color as a parameter.
     In App.js add two ColoredButtons with texts ("Blue Screen", "Yellow Screen"). If you click first button you will show a Blue Screen, second button you will show a Yellow Screen.
   */
-
-
-<ColoredButton onPress={ColoredScreen} color="blue" text="Blue Screen"/>
+  
+  <ColoredButton onPress={ColoredScreen} color="blue" text="Blue Screen"/>
 <ColoredButton onPress={ColoredScreen} color="yellow" text="Yellow Screen"/>
+
 
     
 export default function App() {
   return(
     <Screen1></Screen1> 
   )
+  
 }
