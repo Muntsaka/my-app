75282e991c7007e3186aab3fc945102853294c02
Muntsa <msr@tinet.org>
2021-06-02 10:28:42 +0200

    coloredButton change screens

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { View } from 'react-native';
 import Screen1 from './screens/Screen1';
 
 //Exercise 3
@@ -6,6 +7,13 @@ import Screen1 from './screens/Screen1';
     Create a new Screen component (call it ColoredScreen.js) which is an empty <View/> with background color as a parameter.
     In App.js add two ColoredButtons with texts ("Blue Screen", "Yellow Screen"). If you click first button you will show a Blue Screen, second button you will show a Yellow Screen.
   */
+
+<View style={styles.container}>
+<ColoredButton onPress={ColoredScreen} color="blue" text="Blue Screen"/>
+
+<ColoredButton onPress={ColoredScreen} color="yellow" text="Yellow Screen"/>
+</View>
+    
 export default function App() {
   return(
     <Screen1></Screen1> 
