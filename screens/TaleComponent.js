4e94ba91352b4a6dd3d78c6f17ca5b157fee292e
Muntsa <msr@tinet.org>
2021-06-03 17:37:09 +0200

    TaleComponent.js prova

--- screens/TaleComponent.js
+++ screens/TaleComponent.js
@@ -1,1 +1,11 @@
-Map
+
+import React from 'react';
+import {View} from 'react-native';
+
+const list = () => {
+    return array.map((tales) => {
+        return (
+          <View key={element.name} style={{margin: 15, border: 15}}></View>
+           <View key={element.author} style={{margin: 15, border: 15}></View>
+
+    return <View>{list()}</View>;
