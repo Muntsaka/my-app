b25e91439330e7676814b59d2b373d081e5b47f5
Muntsa <msr@tinet.org>
2021-06-03 15:21:04 +0200

    new Screen2

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -2,14 +2,34 @@
 const Screen2 = ({}) => {
   
   /*Define an array of 5 objects Tale, each tale has properties name and author*/
-  const tales = null;
+  const tales = [
+    {name: 'Conte1',
+    author: 'Pepito1'
+  },
+
+  {name: 'Conte2',
+    author: 'Pepito2'
+  },
+
+  {name: 'Conte3',
+  author: 'Pepito3'
+},
+
+{name: 'Conte4',
+    author: 'Pepito4'
+  },
+
+  {name: 'Conte5',
+    author: 'Pepito5'
+  }
+]
 
   /*Map each element of the array to a TaleComponent (created under /components) using the function map.
     TaleComponent is a component with parameters name and author and visually is a React View with two React Text inside.
     The React View has a border and a margin of 15.
   */
   return( 
-   tales.map((tale) => {/*component goes here*/} )
+   tales.map((tale) => {TaleComponent} )
   );
   
 };
