07922a0339d009a0723bffbedeb6fddf59c1f393
Muntsa <msr@tinet.org>
2021-06-04 10:33:24 +0200

    emboliu

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
-import { setStatusBarBackgroundColor } from 'expo-status-bar';
 import React from 'react';
 import Screen2 from './screens/Screen2'
 
 
+
 //Exercise4
 //Read Screen2.js    
-export default function App() {
-  {Screen2.map((e)=>{
-    return (
-    <Info name={e.name} author={e.author}/>
-  );})}
-}
-export default App;
-  return <Screen2/>
+export default Screen2;
