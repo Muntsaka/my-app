aa6ad62073c970d5f5668657691f910710535d13
Muntsa <msr@tinet.org>
2021-06-04 10:18:33 +0200

    proves vàries

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ import { setStatusBarBackgroundColor } from 'expo-status-bar';
 import React from 'react';
 import Screen2 from './screens/Screen2'
 
+
 //Exercise4
 //Read Screen2.js    
 export default function App() {
-  return <Screen2/>
+  {Screen2.map((e)=>{
+    return (
+    <Info name={e.name} author={e.author}/>
+  );})}
 }
+export default App;
+  return <Screen2/>
--- screens/TaleComponent.js
+++ screens/TaleComponent.js
@@ -1,11 +1,17 @@
 
 import React from 'react';
-import {View} from 'react-native';
-
-const list = () => {
-    return array.map((tales) => {
-        return (
-          <View key={element.name} style={{margin: 15, border: 15}}></View>
-           <View key={element.author} style={{margin: 15, border: 15}></View>
-
-    return <View>{list()}</View>;
+function Info (props){
+    return (
+      <div style={{border:'1px solid black',
+                   margin:'15px',width:'20%'}}>
+         
+  <p> {props.name} </p>
+    
+         
+  <p> {props.author}</p>
+    
+       </div>
+    
+    )
+  }
+  export default Info;
