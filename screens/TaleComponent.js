798dbdb8307ee0d7b8c477a3bc72c1657cff2942
Muntsa <msr@tinet.org>
2021-06-07 15:24:41 +0200

    noves proves TaleComponent

--- screens/TaleComponent.js
+++ screens/TaleComponent.js
@@ -1,17 +1,43 @@
 
 import React from 'react';
-function Info (props){
-    return (
-      <div style={{border:'1px solid black',
-                   margin:'15px',width:'20%'}}>
-         
-  <p> {props.name} </p>
-    
-         
-  <p> {props.author}</p>
-    
-       </div>
-    
-    )
+import {View, Text} from 'react-native';
+import Screen2 from './screens/Screen2'
+
+const Screen2 = () => {
+  const tales = [
+    {name: 'Conte1',
+    author: 'Pepito1'
+  },
+
+  {name: 'Conte2',
+    author: 'Pepito2'
+  },
+
+  {name: 'Conte3',
+  author: 'Pepito3'
+},
+
+{name: 'Conte4',
+    author: 'Pepito4'
+  },
+
+  {name: 'Conte5',
+    author: 'Pepito5'
   }
-  export default Info;
+];
+
+  const list = () => {
+    return tales.map((element) => {
+      return (
+        <View key={element.key} style={{margin: 10}}>
+          <Text>{element.title}</Text>
+          <Text>{element.author}</Text>
+        </View>
+      );
+    });
+  };
+
+  return <View>{list()}</View>;
+};
+
+export default Screen2;
