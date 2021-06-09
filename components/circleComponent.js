a8cff10cf0fc868b94e9ee54f004fddbd6e4ac60
Muntsa <msr@tinet.org>
2021-06-09 11:35:22 +0200

    arreglatperoENBLANC

--- components/circleComponent.js
+++ components/circleComponent.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { View } from 'react';
 
 
-const Circle =
-<View style={{
-    width: 60,
-    height: 60,
-    justifyContent: "center",
-    borderRadius: 60 / 2,
-    backgroundColor: 'orange',
-  }}>
-  </View>
-
-  export default Circle;
+const Circle = ({bgColor}) => {
+return (<View style={{backgroundColor: bgColor, height: 60, width: 60, justifyContent: "center",
+borderRadius: 60 / 2,}}>
+</View>
+);
+}
+    export default Circle;
