bb09d151d2422539fd1ffe688ce334704a03a0c8
Muntsa <msr@tinet.org>
2021-06-09 11:59:34 +0200

    commit arreglos

--- components/circleComponent.js
+++ components/circleComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react';
+import { View } from 'react-native';
 
 
-const Circle = ({bgColor}) => {
+const CircleComponent = ({bgColor}) => {
 return (
 <View style={{backgroundColor: bgColor, height: 60, width: 60, justifyContent: "center",
 borderRadius: 60 / 2,}}>
 </View>
 )
 }
-    export default Circle;
+    export default CircleComponent;
