20d9028a340dc83db4dd0f825baf8fac041271e2
Muntsa <msr@tinet.org>
2021-06-14 15:46:53 +0200

    commit nou

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -8,14 +8,13 @@ const Screen3 = ({ }) => {
   const CircleArray = () => {
     const array = [];
     for (let i = 0; i < 100; i++) {
-      const circle = color('#d5d5d5' , '#a9a9a9');
+      const circle = (color="blue");
       array.push(circle);
     }
     return array
   }
-}
+
   return (CircleArray().map((element) => <CircleComponent bgColor={element.color} />)
   );
-
-
+}
 export default Screen3;
