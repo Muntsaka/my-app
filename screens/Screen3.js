2060540bc130e6ec8d9eaf416753093aa5cf9124
Muntsa <msr@tinet.org>
2021-06-14 14:33:18 +0200

    Screen 3 nou

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,42 +1,20 @@
-import ReactDOM from 'react-dom'
+
 import React from 'react'
-import CircleComponent from '../components/CircleComponent';
  
 
-const Screen3 = ({}) => 
+const Screen3 = ({}) => {
   
-class CircleComponent extends React.Component {
-  render() {
-    var circleStyle = {
-      padding: 10,
-      margin: 20,
-      display: "inline-block",
-      backgroundColor: this.props.bgColor,
-      borderRadius: "50%",
-      width: 100,
-      height: 100
-    };
-    return (
-      <div style={circleStyle}>
-      </div>
-    );
-  }
+const CircleArray = () => {
+const Array = [];
+for (let i=0; i<100; i++) {
+const circle = (color="blue");
+array.push(circle);
+}
+return Array
 }
 
-var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363", "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
-
-var renderData = [];
-
-for (var i = 0; i < colors.length; i++) {
-  var color = colors[i];
-  renderData.push(<CircleComponent key={i + color} bgColor={color}/>);
+return (CircleArray().map((color) => <CircleComponent color={color.color}/>)
+);
 }
-
-ReactDOM.render(
-    <div>
-    {renderData}
-    </div>,
-    document.querySelector("#container")
-  );
 
 export default Screen3;
