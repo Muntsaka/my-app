8a78bd75291ec3aaf3cfa082c071a2d1a40ed749
Muntsa <msr@tinet.org>
2021-06-14 16:48:07 +0200

    iltim

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -8,7 +8,8 @@ const Screen3 = ({ }) => {
   const CircleArray = () => {
     const array = [];
     for (let i = 0; i < 100; i++) {
-      const circle = (color = "blue");
+      const circle = {color};
+      let color = ["red", "lightblue", "pink", "yellow", "brown", "magenta"];
       array.push(circle);
     }
     return array
