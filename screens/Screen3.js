c573e1d5515cc755e3098500ce7cfa9837e8d6d0
Muntsa <msr@tinet.org>
2021-06-14 14:52:12 +0200

    colorets

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,21 +1,21 @@
 
 import React from 'react'
 import CircleComponent from '../components/CircleComponent';
- 
 
-const Screen3 = ({}) => {
-  
-const CircleArray = () => {
-const array = [];
-for (let i=0; i<100; i++) {
-const circle = {color:"blue"};
-array.push(circle);
-}
-return array
-}
 
-return (CircleArray().map((element) => <CircleComponent bgColor={element.color}/>)
-);
+const Screen3 = ({ }) => {
+
+  const CircleArray = () => {
+    const array = [];
+    for (let i = 0; i < 100; i++) {
+      const circle = { color1: "blue", color2: "red" };
+      array.push(circle);
+    }
+    return array
+  }
+
+  return (CircleArray().map((element) => <CircleComponent bgColor={element.color1} bgColor={element.color2} />)
+  );
 }
 
 export default Screen3;
