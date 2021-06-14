0fe5dbb5fbdb8415fb631de0f80cb699e03923db
Muntsa <msr@tinet.org>
2021-06-14 09:49:24 +0200

    Creat un cercle!

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import CircleComponent from './components/Circlecomponent'
-
-const Screen2 = ({}) => {
-
-  const Circle = <Circle center={[0, 0]} radius={7} color={"red"} />
-
-  return (Circle.map => {
-  <Circlecomponent bgColor="blue"/>;
-
-};
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 
-export default Screen3;
+export default class App extends Component {
 
-/*STEP-1  Create a CircleComponent with a paramter color in /components. The component is a React Native <View/> with a specific style (Css) that looks like a perfect circle.
-            Show ONE circle in the screen and make the app work (and commit)
-  */
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.CircleShape} />
+      </View>
+    );
+  }
+}
 
-  /*STEP-2  Define an array of 100 objects Circle, each Circle has property color and make the app work)
-            Map each element of the array to a CircleComponent with color blue using the function map and make the app work (and commit)
-  */
-  /*STEP-3  Paint even circles blue and odds circle green, make the app work (and commit).
-  */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e5e5e5",
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  }, 
+  CircleShape: {
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+    backgroundColor: '#FF9800',
+  },
+
+});
