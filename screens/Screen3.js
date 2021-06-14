beb399f59cf18a41e727b459cd4578fb99ecd199
Muntsa <msr@tinet.org>
2021-06-14 11:25:11 +0200

    Screen3

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
+import CircleComponent from '../components/CircleComponent';
  
 
 const Screen3 = ({}) => 
-{
-  class Circle extends React.Component {
+  
+class CircleComponent extends React.Component {
   render() {
     var circleStyle = {
       padding: 10,
@@ -15,29 +16,27 @@ const Screen3 = ({}) =>
       width: 100,
       height: 100
     };
-
-    var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363",
-          "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
-
-    var renderData = [];
-
-    for (var i = 0; i < colors.length; i++) {
-      var color = colors[i];
-      renderData.push(<Circle key={i + color} bgColor={color}/>);
-    };
-    
     return (
-        <div style={circleStyle}>
-        </div>
+      <div style={circleStyle}>
+      </div>
     );
-    }
   }
+}
 
-  ReactDOM.render(
-   <div>
+var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363", "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
+
+var renderData = [];
+
+for (var i = 0; i < colors.length; i++) {
+  var color = colors[i];
+  renderData.push(<CircleComponent key={i + color} bgColor={color}/>);
+}
+
+ReactDOM.render(
+    <div>
     {renderData}
-   </div>,
-   document.querySelector("#container")
+    </div>,
+    document.querySelector("#container")
   );
-  }
+
 export default Screen3;
