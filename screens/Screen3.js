b9f7a0beaa4be7d9e0970d62c3790f81c79a4b05
Muntsa <msr@tinet.org>
2021-06-15 10:47:31 +0200

    final

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -6,20 +6,22 @@ import CircleComponent from '../components/CircleComponent';
 const Screen3 = ({ }) => {
 
   const CircleArray = () => {
-    const array = [{ color: "blue" }, { color: "green" }, { color: "blue" }, { color: "green" }];
-    for (let i = 0; i < 100; i++) {
-      if(i % 2 == 0) {
-        color = "blue"
-    } else {
-        color = "green"
+    const array = [];
+    for (let i=0; i<100; i++){
+      if(i%2==0){
+        const newcircle = {color:"blue"};
+        array.push(newcircle);
+      } else{
+        const newcircle = {color:"green"};
+        array.push(newcircle);
+      }
     }
-    const circle = { color };
-    array.push(circle);
 
   return array
 }
 
 return (CircleArray().map((element) => <CircleComponent bgColor={element.color} />)
 );
-  }
+};
+
 export default Screen3;
