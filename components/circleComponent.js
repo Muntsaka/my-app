aa2e2017c6f5d6e3f9064c4f1dc8ec256e126c7b
Muntsa <msr@tinet.org>
2021-06-16 10:43:07 +0200

    onpress

--- components/circleComponent.js
+++ components/circleComponent.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native'; 
+import { StyleSheet } from 'react-native';
 
 
 const CircleComponent = ({bgColor}) => {
-return (
-<View style={{backgroundColor: bgColor, height: 60, width: 60, justifyContent: "center",
-borderRadius: 60 / 2}}>
-</View>
-)
+return <TouchableOpacity
+style={styles.button}
+onPress={() => console.log('COLOR')}
+>
+</TouchableOpacity>
+};
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+justifyContent: "center",
+paddingHorizontal: 10
+},
+button: {
+alignItems: "center",
+backgroundColor: "#DDDDDD",
+padding: 10
+},
+countContainer: {
+alignItems: "center",
+padding: 10
 }
+});
+
+
+
     export default CircleComponent;
