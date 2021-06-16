a109b0ec25c631db2d7fcdbff2c7a731df644805
Muntsa <msr@tinet.org>
2021-06-16 10:51:33 +0200

    commit styles

--- components/circleComponent.js
+++ components/circleComponent.js
@@ -7,27 +7,11 @@ import { StyleSheet } from 'react-native';
 const CircleComponent = ({bgColor}) => {
 return <TouchableOpacity
 style={styles.button}
-onPress={() => console.log('COLOR')}
+onPress={(bgColor) => console.log('COLOR')}
 >
 </TouchableOpacity>
 };
 
-const styles = StyleSheet.create({
-container: {
-flex: 1,
-justifyContent: "center",
-paddingHorizontal: 10
-},
-button: {
-alignItems: "center",
-backgroundColor: "#DDDDDD",
-padding: 10
-},
-countContainer: {
-alignItems: "center",
-padding: 10
-}
-});
 
 
 
