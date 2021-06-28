596d9de8fb1378f23d7c3fbf1977556804217e92
Muntsa <msr@tinet.org>
2021-06-28 13:07:03 +0200

    canvis ButtonTask

--- components/buttontask.tsx
+++ components/buttontask.tsx
@@ -1,12 +1,16 @@
 
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 const ButtonTask = (props:{onPress?:()=>void, color:string, text:string}) => {
 return(
-  <view onPress={props.onPress} style={{backgroundColor:"blue", padding:10, margin:10}} >
+  <View>
+      <Button
+        title="Afegeix"
+        onPress={props.onPress} style={{backgroundColor:"blue", padding:10, margin:10}}
+      />
     <Text style={{color:props.color}}>{props.text}</Text>
-  </view>)
+  </View>)
 }
 
 
