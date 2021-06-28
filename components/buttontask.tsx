9149ad7f7e716e014f93fa0f8763a3ae1e689bbe
Muntsa <msr@tinet.org>
2021-06-28 11:39:35 +0200

    update

--- components/buttontask.tsx
+++ components/buttontask.tsx
@@ -4,7 +4,7 @@ import { Text } from 'react-native';
 
 const ButtonTask = (props:{onPress:()=>void, color:string, text:string}) => {
 return(
-  <view onPress={props.onPress} style={{backgroundColor:"grey", padding:10, margin:10}} >
+  <view onPress={props.onPress} style={{backgroundColor:"blue", padding:10, margin:10}} >
     <Text style={{color:props.color}}>{props.text}</Text>
   </view>)
 }
