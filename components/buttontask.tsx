0311c13c3e5a4d73fd8c31ad027235971ba72006
Ramon Salla <rsallar@gmail.com>
2021-07-10 20:06:23 +0200

    added dynamic textDecoration style

--- components/buttontask.tsx
+++ components/buttontask.tsx
@@ -2,19 +2,16 @@
 import React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 
-const ButtonTask = (props:{onPress?:()=>void, color:string, text:string, pos:number, textDecorationLine:true}) => {
+const ButtonTask = (props:{onPress?:()=>void, color:string, text:string, pos:number, complete:boolean}) => {
 return(
   <View style= {{margin:10, flex: 1, flexDirection: "row"}}>
-    <Text style={{color:props.color}}>{props.pos + "-" + props.text} </Text>
-    <Text style={{textDecorationLine: 'line-through'}}>{props.pos + "-" + props.text}
-
-</Text>
-    
-      <TouchableOpacity onPress={props.onPress} ><Text>Esborra</Text>
-      </TouchableOpacity>
-  
+    <Text style={{color:props.text==="alert"?"red":props.color, textDecorationLine:props.pos==0?"line-through":"none"}}>
+      {props.pos + "-" + props.text} 
+    </Text>
+    <TouchableOpacity onPress={props.onPress} >
+      <Text>Esborra</Text>
+    </TouchableOpacity>
   </View>)
 }
-
 
 export default ButtonTask;
