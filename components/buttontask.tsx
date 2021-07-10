5b23babd43af0de9d0b8a95cd0d2c602f41ccbde
Ramon Salla <rsallar@gmail.com>
2021-07-10 20:07:12 +0200

    props.complete

--- components/buttontask.tsx
+++ components/buttontask.tsx
@@ -5,7 +5,7 @@ import { TouchableOpacity, Text, View } from 'react-native';
 const ButtonTask = (props:{onPress?:()=>void, color:string, text:string, pos:number, complete:boolean}) => {
 return(
   <View style= {{margin:10, flex: 1, flexDirection: "row"}}>
-    <Text style={{color:props.text==="alert"?"red":props.color, textDecorationLine:props.pos==0?"line-through":"none"}}>
+    <Text style={{color:props.text==="alert"?"red":props.color, textDecorationLine:props.complete?"line-through":"none"}}>
       {props.pos + "-" + props.text} 
     </Text>
     <TouchableOpacity onPress={props.onPress} >
